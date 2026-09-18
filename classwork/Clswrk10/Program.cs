1feefe939f9dcd51706f670b4a9d63712bdfe285
Adithya S <adithyaaduzz123@gmail.com>
2026-09-18 12:52:20 +0530

    Add .NET 10 project with Vehicle class hierarchy
    Added Clswrk10.csproj targeting .NET 10.0 with nullable enabled and executable output. Created Clswrk10.slnx referencing the project. Implemented Vehicle base class with brand property and virtual GetDetails. Added derived classes: car (with model property, lowercase name) and Truck (with LoadCapacity). Main method demonstrates usage by printing details of car and Truck instances.

---
+++ classwork/Clswrk10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+class Vehicle
+{
+    public string brand { get; set; }
+
+    public Vehicle()
+        {
+        brand= "unknown";
+    }
+    public virtual string GetDetails()
+    {
+        return "Brand: " + brand;
+    }
+
+}
+class car : Vehicle
+{
+    public string model { get; set; }
+    public car(string brand, string model)
+    {
+        this.brand = brand;
+        this.model = model;
+    }
+    public override string GetDetails()
+    {
+        return "Car Brand: " + brand + ", Model: " + model;
+    }
+}
+class Truck : Vehicle
+{
+    public double LoadCapacity { get; set; }
+
+    // Constructor
+    public Truck(string brand, double loadCapacity)
+    {
+        this.brand = brand;
+        this.LoadCapacity = loadCapacity;
+    }
+
+    // Override method
+    public override string GetDetails()
+    {
+        return "Truck Brand: " + brand +
+               ", Load Capacity: " + LoadCapacity + " tons";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        car car = new car("Toyota", "Corolla");
+
+        Truck truck = new Truck("Volvo", 15.5);
+
+        Console.WriteLine(car.GetDetails());
+        Console.WriteLine(truck.GetDetails());
+    }
+}
+
+
+
+
