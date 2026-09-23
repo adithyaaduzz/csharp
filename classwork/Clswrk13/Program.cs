2823a10caac994c1aeb211c86999cb51b0db809e
Adithya S <adithyaaduzz123@gmail.com>
2026-09-23 12:02:06 +0530

    Current Date and Future Date

---
+++ classwork/Clswrk13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+ class program
+{
+    static void Main()
+    {
+        try
+        {
+            DateTime CurrentDate = DateTime.Now;
+            DateTime Futuredate = CurrentDate.AddDays(5);
+
+            TimeSpan Difference = Futuredate - CurrentDate;
+            int DaysLeft = Difference.Days;
+
+            string result = $"Current Date: {CurrentDate}\nFuture Date: {Futuredate}\nDays Left: {DaysLeft}";
+            Console.WriteLine("Result:\n" + result);
+        
+         File.WriteAllText("date_result.txt", result);
+
+        Console.WriteLine("Calculation successful! Result saved to date_result.txt");
+    }
+        catch (Exception ex)
+        {
+           
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
+       
+
+    
