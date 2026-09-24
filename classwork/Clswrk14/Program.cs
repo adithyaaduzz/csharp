9740d276674c6c1451f4e0a566aaa3554df9c42a
Adithya S <adithyaaduzz123@gmail.com>
2026-09-24 12:34:28 +0530

    Threading

---
+++ classwork/Clswrk14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Threading;
+class program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(1000);
+        }
+    }
+    static void PrintLetters()
+    {
+        for (char j = 'A'; j <= 'E'; j++)
+        {
+            Console.WriteLine(j);
+            Thread.Sleep(1000);
+        }
+    }
+    static void Main()
+    {
+        try
+        {
+            Thread Num = new Thread(PrintNumbers);
+            Thread Letter = new Thread(PrintLetters);
+            Num.Start();
+            Letter.Start();
+
+            Num.Join();
+            Letter.Join();
+
+        }
+    catch(Exception Ex)
+    {
+            Console.WriteLine("An error occurred: " + Ex.Message);
+        }finally{
+            Console.WriteLine("Execution completed.");
+
+        }
+    }
+
+}
+
